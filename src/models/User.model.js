95c5dfbd3e2af24d0d0e855d1edebba1f1a02495
vitalii <vilavren@gmail.com>
2023-11-02 02:36:30 +0500

    added: check token

---
+++ src/models/User.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    passwordHash: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model('User', UserSchema)
