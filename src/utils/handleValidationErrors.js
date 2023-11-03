cb8c622a9969bcab5aabf0c07d04de6fc41812e7
vitalii <vilavren@gmail.com>
2023-11-03 18:57:02 +0500

    fix: validations

--- src/utils/handleValidationErrors.js
+++ src/utils/handleValidationErrors.js
@@ -1,6 +1,6 @@
 import { validationResult } from 'express-validator'
 
-export const handleValidationErrors = (res, req, next) => {
+export const handleValidationErrors = (req, res, next) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array())
